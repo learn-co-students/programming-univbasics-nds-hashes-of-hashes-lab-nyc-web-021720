a04d728479169d40d57a1479f3f45db4d2f6b581
Samantha Ponce <samanthaponce5@gmail.com>
2020-02-06 22:43:06 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -54,23 +54,25 @@
 
   def naming_system
 new_hash = {
+  
    label: "Kingdom",
    sub_category: {
      label: "Phylum",
      sub_category: {
        label: "Class",
-  sub_category:{
-   label: "Order",
- sub_category:{
-   label: "Family",
-   sub_category: {
-     label: "Genus",
-  sub_category: {
-   label: "Species",
-   sub_category: nil
- }}}}}}}
-  
-    
+       sub_category:{
+         label: "Order",
+         sub_category:{
+          label: "Family",
+            sub_category: {
+              label: "Genus",
+              sub_category:{
+              label: "Species",
+              }}}}}}}
+
+ 
+ 
+ 
 
 end
 
